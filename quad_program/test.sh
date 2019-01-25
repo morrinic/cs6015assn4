b0e26633a1dc094ad6bdd1fc6d2bfc773ad994da
morrinic <nicole.morris@utah.edu>
2019-01-25 12:13:19 -0700

    make test seems to be working, added travis files

--- quad_program/test.sh
+++ quad_program/test.sh
@@ -1,8 +1,2 @@
-./main ./kite1.txt | grep 'kite' &&
-./main ./kite2.txt | grep 'kite' &&
-./main ./quad.txt | grep 'quadrilateral' &&
-./main ./rectangle.txt | grep 'rectangle' &&
-./main ./rhombus.txt | grep 'rhombus' &&
-./main ./square.txt | grep 'square' &&
-./main ./trapezoid1.txt | grep 'trapezoid' &&
-./main ./trapezoid2.txt | grep 'trapezoid'
+./main <  test_data.txt > output_results.txt
+diff output_results.txt test_results.txt
