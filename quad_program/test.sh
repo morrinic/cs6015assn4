ad7ecab2e12cbc8306b3e287e648093af97ed6bb
morrinic <nicole.morris@utah.edu>
2019-01-25 11:08:03 -0700

    Ran line coverage/added tests; all lines covered

--- quad_program/test.sh
+++ quad_program/test.sh
@@ -1,6 +1,8 @@
-./main ./kite.txt | grep 'kite' &&
+./main ./kite1.txt | grep 'kite' &&
+./main ./kite2.txt | grep 'kite' &&
 ./main ./quad.txt | grep 'quadrilateral' &&
 ./main ./rectangle.txt | grep 'rectangle' &&
 ./main ./rhombus.txt | grep 'rhombus' &&
 ./main ./square.txt | grep 'square' &&
-./main ./trapezoid.txt | grep 'trapezoid'
+./main ./trapezoid1.txt | grep 'trapezoid' &&
+./main ./trapezoid2.txt | grep 'trapezoid'
