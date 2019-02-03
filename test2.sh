b37f28a30b8921eb41f3b482ecb706381540fa9a
morrinic <nicole.morris@utah.edu>
2019-02-02 17:43:45 -0700

    Updating testing errors

---
+++ test2.sh
@@ -0,0 +1,8 @@
+./main < ./kite1.txt | grep 'kite' &&
+./main < ./kite2.txt | grep 'kite' &&
+./main < ./quad.txt | grep 'quadrilateral' &&
+./main < ./rectangle.txt | grep 'rectangle' &&
+./main < ./rhombus.txt | grep 'rhombus' &&
+./main < ./square.txt | grep 'square' &&
+./main < ./trapezoid1.txt | grep 'trapezoid' &&
+./main < ./trapezoid2.txt | grep 'trapezoid'
