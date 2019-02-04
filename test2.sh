5a6b17bd9cea3ac6fbc23f698711963f5e6d90c0
morrinic <nicole.morris@utah.edu>
2019-02-04 12:17:30 -0700

    Updated test2.sh to include all error checks

--- test2.sh
+++ test2.sh
@@ -5,4 +5,11 @@
 ./main < txt_files/square.txt | grep 'square' &&
 ./main < txt_files/trapezoid1.txt | grep 'trapezoid' &&
 ./main < txt_files/trapezoid2.txt | grep 'trapezoid' &&
-./main < txt_files/error1_add_points.txt | grep 'error 1'
+./main < txt_files/error1_add_points.txt | grep 'error 1' &&
+./main < txt_files/error1_sub_points.txt | grep 'error 1' &&
+./main < txt_files/error1_high_point.txt | grep 'error 1' &&
+./main < txt_files/error1_low_point.txt | grep 'error 1' &&
+./main < txt_files/error1_letter_point.txt | grep 'error 1' &&
+./main < txt_files/error2_point_collision.txt | grep 'error 2' &&
+./main < txt_files/error3_line_collision.txt | grep 'error 3' &&
+./main < txt_files/error4_collinear.txt | grep 'error 4'
