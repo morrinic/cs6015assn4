c41ef6996f314c61d36f13536242a91e24578981
morrinic <nicole.morris@utah.edu>
2019-02-04 12:10:20 -0700

    Updated rhombus

--- test2.sh
+++ test2.sh
@@ -1,8 +1,7 @@
-./main < ./txt_files/kite1.txt | grep 'kite' &&
-./main < ./txt_files/kite2.txt | grep 'kite' &&
-./main < ./txt_files/quad.txt | grep 'quadrilateral' &&
-./main < ./txt_files/rectangle.txt | grep 'rectangle' &&
-./main < ./txt_files/rhombus.txt | grep 'rhombus' &&
-./main < ./txt_files/square.txt | grep 'square' &&
-./main < ./txt_files/trapezoid1.txt | grep 'trapezoid' &&
-./main < ./txt_files/trapezoid2.txt | grep 'trapezoid'
+./main < txt_files/kite1.txt | grep 'kite' &&
+./main < txt_files/quad.txt | grep 'quadrilateral' &&
+./main < txt_files/rectangle.txt | grep 'rectangle' &&
+./main < txt_files/rhombus.txt | grep 'rhombus' &&
+./main < txt_files/square.txt | grep 'square' &&
+./main < txt_files/trapezoid1.txt | grep 'trapezoid' &&
+./main < txt_files/trapezoid2.txt | grep 'trapezoid'
